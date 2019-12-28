463b215f2b31c7d1f171b3c6858c5c43afc3c6d1
unknown <mejia.alan.r@gmail.com>
2019-12-28 16:27:09 -0300

    Updates

---
+++ precache-manifest.de1451b42493720358d94319cfac923a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150138f5faefecf727f49bc6b593744e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b79b955e3872a72c3331",
+    "url": "/monster-rolodex/static/css/main.d506b2f3.chunk.css"
+  },
+  {
+    "revision": "3cf616a94219505cb0ec",
+    "url": "/monster-rolodex/static/js/2.632a08d1.chunk.js"
+  },
+  {
+    "revision": "b79b955e3872a72c3331",
+    "url": "/monster-rolodex/static/js/main.54fc8e67.chunk.js"
+  },
+  {
+    "revision": "911cf27a028f86e95b2c",
+    "url": "/monster-rolodex/static/js/runtime-main.5867cbb8.js"
+  }
+]);
